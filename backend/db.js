b4281824e700d5498f9b06992e2746767e8a2c58
Md Abubakar <58105090+MdAbubakar@users.noreply.github.com>
2023-02-25 18:02:02 +0530

    iNotebook Updated

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI =
+  "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
 mongoose.set("strictQuery", true);
+
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongo Successfully");
--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ const express = require("express");
 connectToMongo();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`The iNotebook app listening at http://localhost:${port}`);
 });
